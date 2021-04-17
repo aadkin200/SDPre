73accc91483c5866a1a5861bbd67866ee290ca09
Alex Adkins <alex.t.adkins@gmail.com>
2021-04-17 14:01:28 -0400

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Alex Adkins";
+    int age = 28;
+    String state = "Alabama";
+    System.out.println("My name is " + name + ", " + "I'm " + age);
+    System.out.println("years old, and im from " + state + "!");
+  }
+}

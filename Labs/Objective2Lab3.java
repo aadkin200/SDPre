e5704f1ce250f47398199e2d58264da18f6b2f18
Alex Adkins <alex.t.adkins@gmail.com>
2021-04-17 14:07:25 -0400

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 23;
+    num2 = 45;
+    num3 = 17;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}

ad0e368d2237b24d3560d0753fc1feed21af52c0
Alex Adkins <alex.t.adkins@gmail.com>
2021-04-18 00:40:58 -0400

    complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int sumTotal = 0;
+    int counter = 0;
+
+    while (counter <= 20) {
+      sumTotal += counter;
+      counter++;
+    }
+    System.out.println(sumTotal);
+  }
+}
